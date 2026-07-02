76bfd2cceb841686a1fb7664aebe0cbfa6f75986
Marilize <marilizef.dev@gmail.com>
2026-07-02 13:34:58 +0200

    feat: add check for api limit reached

--- src/api/http.ts
+++ src/api/http.ts
@@ -1,5 +1,10 @@
 const http = async <T>(url: string): Promise<T> => {
     const response = await fetch(url);
+    
+    // [429] too_many_requests: User has reached their subscription's monthly request allowance.
+    if (response.status === 429) {
+        throw new Error("Monthly request limit reached. Please try again later.");
+    }
 
     if (!response.ok) {
         throw new Error("Failed to fetch data");
